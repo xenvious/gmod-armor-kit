9be482e009941faeac816e9423fe383c342a09f7
xenvious <vindictful@gmail.com>
2016-12-12 18:57:10 -0500

    Update shared.lua

--- armor/shared.lua
+++ armor/shared.lua
@@ -1,8 +1,8 @@
 DEFINE_BASECLASS( "base_gmodentity" )
 
 ENT.PrintName		= "Armor"
-ENT.Author		    = "Xenvious"
-ENT.Information		= "Armor"
+ENT.Author		  = "Xenvious"
+ENT.Information	= "Armor"
 ENT.Category		= "TKServers"
 
 ENT.Editable		= false
