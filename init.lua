5c9c4c262c6d7f7de69209a139ee60bd888f7f02
xenvious <vindictful@gmail.com>
2016-12-12 18:47:50 -0500

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,41 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+include('shared.lua')
+
+function ENT:Initialize()
+	self.Entity:SetModel("models/Items/BoxMRounds.mdl")
+	self.Entity:PhysicsInit(SOLID_VPHYSICS)
+	self.Entity:SetMoveType(MOVETYPE_VPHYSICS)
+	self.Entity:SetSolid(SOLID_VPHYSICS)
+	self:SetUseType(SIMPLE_USE)
+	
+	local phys = self.Entity:GetPhysicsObject()
+	self.nodupe = true
+	self.ShareGravgun = true
+
+	if phys and phys:IsValid() then phys:Wake() end
+	self:SetCollisionGroup(COLLISION_GROUP_INTERACTIVE_DEBRIS) 
+end
+
+function ENT:SpawnFunction(ply, tr)
+   	if (!tr.Hit) then
+	return end
+ 	 
+ 	local SpawnPos = tr.HitPos + tr.HitNormal
+
+ 	local ent = ents.Create("Armor")
+	ent:SetPos(SpawnPos)
+	ent:Spawn()
+	ent:Activate()
+ 	 	 
+	return ent 
+end
+
+function ENT:Use(plyUse)
+	self:Remove()
+	plyUse:SetArmor(250)
+end
+
+function ENT:OnRemove()
+	self:Remove()
+end
