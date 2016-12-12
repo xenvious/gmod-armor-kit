350d6f006c44a898648d2fc3a8f9a35799183b93
xenvious <vindictful@gmail.com>
2016-12-12 18:53:26 -0500

    Add files via upload

---
+++ armor/shared.lua
@@ -0,0 +1,10 @@
+DEFINE_BASECLASS( "base_gmodentity" )
+
+ENT.PrintName		= "Armor"
+ENT.Author		    = "Reidmaster"
+ENT.Information		= "Armor"
+ENT.Category		= "TKServers"
+
+ENT.Editable		= false
+ENT.Spawnable		= true
+ENT.AdminOnly		= true
